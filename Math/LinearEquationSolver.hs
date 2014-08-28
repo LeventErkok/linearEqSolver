5fe69422829b4dec15ba39099879c50dd1ac4c5d
Levent Erkok <erkokl@gmail.com>
2014-08-27 18:49:01 -0700

    Use Solvers from SBV release

--- Math/LinearEquationSolver.hs
+++ Math/LinearEquationSolver.hs
@@ -15,7 +15,7 @@
 
 module Math.LinearEquationSolver (
        -- * Available SMT solvers
-       z3, cvc4, Solver(..)
+       Solver(..)
        -- * Solutions over Integers
     ,  solveIntegerLinearEqs
     ,  solveIntegerLinearEqsAll
@@ -24,21 +24,7 @@ module Math.LinearEquationSolver (
     ,  solveRationalLinearEqsAll
     ) where
 
-import           Data.SBV hiding (z3, cvc4)
-import qualified Data.SBV as SBV (z3, cvc4)
-
--- | Interface to the SMT solver, via the SBV library. Unless you need to create
--- custom solvers, or tweak the existing ones, you should not need to use this type: Simply
--- use the values 'z3' and 'cvc4' to access the respective SMT solvers.
-newtype Solver = Solver SMTConfig
-
--- | The Z3 SMT solver from Microsoft: <http://z3.codeplex.com/>.
-z3 :: Solver
-z3 = Solver SBV.z3
-
--- | The CVC4 SMT solver from New York University and the University of Iowa: <http://cvc4.cs.nyu.edu>.
-cvc4 :: Solver
-cvc4 = Solver SBV.cvc4
+import Data.SBV
 
 -- | Solve a system of linear integer equations. The first argument is
 -- the matrix of coefficients, known as @A@, of size @mxn@. The second argument
@@ -54,7 +40,7 @@ cvc4 = Solver SBV.cvc4
 --     2x      +  z = 8
 -- @
 --
--- >>> solveIntegerLinearEqs z3 [[2, 3, 4],[6, -3, 9],[2, 0, 1]] [20, -6, 8]
+-- >>> solveIntegerLinearEqs Z3 [[2, 3, 4],[6, -3, 9],[2, 0, 1]] [20, -6, 8]
 -- Just [5,6,-2]
 --
 -- The first argument picks the SMT solver to use. Valid values are 'z3' and
@@ -62,16 +48,16 @@ cvc4 = Solver SBV.cvc4
 --
 -- In case there are no solutions, we will get `Nothing`:
 --
--- >>> solveIntegerLinearEqs z3 [[1], [1]] [2, 3]
+-- >>> solveIntegerLinearEqs Z3 [[1], [1]] [2, 3]
 -- Nothing
 --
 -- Note that there are no solutions to this second system as it stipulates the unknown is
 -- equal to both 2 and 3. (Overspecified.)
-solveIntegerLinearEqs :: Solver                -- ^ SMT Solver to use, pass one of 'z3' or 'cvc4'
+solveIntegerLinearEqs :: Solver                -- ^ SMT Solver to use
                       -> [[Integer]]           -- ^ Coefficient matrix (A)
                       -> [Integer]             -- ^ Result vector (b)
                       -> IO (Maybe [Integer])  -- ^ A solution to @Ax = b@, if any
-solveIntegerLinearEqs (Solver cfg) coeffs res = extractModel `fmap` satWith cfg cs
+solveIntegerLinearEqs cfg coeffs res = extractModel `fmap` satWith (getSolver cfg) cs
   where cs = buildConstraints "solveIntegerLinearEqs" coeffs res
 
 -- | Similar to `solveIntegerLinearEqs`, except returns all possible solutions.
@@ -90,13 +76,13 @@ solveIntegerLinearEqs (Solver cfg) coeffs res = extractModel `fmap` satWith cfg 
 --
 -- We have:
 --
--- >>> take 3 `fmap` solveIntegerLinearEqsAll z3 [[2, 3, 4],[6, -3, 9]] [20, -6]
+-- >>> take 3 `fmap` solveIntegerLinearEqsAll Z3 [[2, 3, 4],[6, -3, 9]] [20, -6]
 -- [[5,6,-2],[-8,4,6],[18,8,-10]]
-solveIntegerLinearEqsAll :: Solver          -- ^ SMT Solver to use, pass one of 'z3' or 'cvc4'
+solveIntegerLinearEqsAll :: Solver          -- ^ SMT Solver to use
                          -> [[Integer]]     -- ^ Coefficient matrix (A)
                          -> [Integer]       -- ^ Result vector (b)
                          -> IO [[Integer]]  -- ^ All solutions to @Ax = b@
-solveIntegerLinearEqsAll (Solver cfg) coeffs res = extractModels `fmap` allSatWith cfg cs
+solveIntegerLinearEqsAll cfg coeffs res = extractModels `fmap` allSatWith (getSolver cfg) cs
   where cs = buildConstraints "solveIntegerLinearEqsAll" coeffs res
 
 -- | Solve a system of linear equations over rationals. Same as the integer
@@ -110,13 +96,13 @@ solveIntegerLinearEqsAll (Solver cfg) coeffs res = extractModels `fmap` allSatWi
 --     7.2x - 5y   = -8.5
 -- @
 --
--- >>> solveRationalLinearEqs z3 [[2.4, 3.6],[7.2, -5]] [12, -8.5]
+-- >>> solveRationalLinearEqs Z3 [[2.4, 3.6],[7.2, -5]] [12, -8.5]
 -- Just [245 % 316,445 % 158]
-solveRationalLinearEqs :: Solver                  -- ^ SMT Solver to use, pass one of 'z3' or 'cvc4'
+solveRationalLinearEqs :: Solver                  -- ^ SMT Solver to use
                        -> [[Rational]]            -- ^ Coefficient matrix (A)
                        -> [Rational]              -- ^ Result vector (b)
                        -> IO (Maybe [Rational])   -- ^ A solution to @Ax = b@, if any
-solveRationalLinearEqs (Solver cfg) coeffs res = (fmap from . extractModel) `fmap` satWith cfg cs
+solveRationalLinearEqs cfg coeffs res = (fmap from . extractModel) `fmap` satWith (getSolver cfg) cs
   where to   = map (fromRational :: Rational -> AlgReal)
         from = map (toRational   :: AlgReal -> Rational)
         cs   = buildConstraints "solveRationalLinearEqs" (map to coeffs) (to res)
@@ -132,13 +118,13 @@ solveRationalLinearEqs (Solver cfg) coeffs res = (fmap from . extractModel) `fma
 --
 -- In this case, the system has infinitely many solutions. We can compute three of them as follows:
 --
--- >>> take 3 `fmap` solveRationalLinearEqsAll z3 [[2.4, 3.6]] [12]
+-- >>> take 3 `fmap` solveRationalLinearEqsAll Z3 [[2.4, 3.6]] [12]
 -- [[0 % 1,10 % 3],[(-3) % 2,13 % 3],[(-3) % 4,23 % 6]]
-solveRationalLinearEqsAll :: Solver             -- ^ SMT Solver to use, pass one of 'z3' or 'cvc4'
+solveRationalLinearEqsAll :: Solver             -- ^ SMT Solver to use
                           -> [[Rational]]       -- ^ Coefficient matrix (A)
                           -> [Rational]         -- ^ Result vector (b)
                           -> IO [[Rational]]    -- ^ All solutions to @Ax = b@
-solveRationalLinearEqsAll (Solver cfg) coeffs res = (map from . extractModels) `fmap` allSatWith cfg cs
+solveRationalLinearEqsAll cfg coeffs res = (map from . extractModels) `fmap` allSatWith (getSolver cfg) cs
   where to   = map (fromRational :: Rational -> AlgReal)
         from = map (toRational   :: AlgReal -> Rational)
         cs   = buildConstraints "solveRationalLinearEqsAll" (map to coeffs) (to res)
@@ -154,3 +140,11 @@ buildConstraints f coeffs res
        solve $ zipWith rowEq (map (map literal) coeffs) (map literal res)
  where m    = length coeffs
        n:ns = map length coeffs
+
+-- | Determine the solver config.
+getSolver :: Solver -> SMTConfig
+getSolver Z3        = z3
+getSolver Yices     = yices
+getSolver Boolector = boolector
+getSolver CVC4      = cvc4
+getSolver MathSAT   = mathSAT
